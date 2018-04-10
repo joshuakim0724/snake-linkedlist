7b883d2b6d3f1268c914afcbdc718efde8ebf178
joshuakim0724 <joshuakim0724@gmail.com>
2018-04-09 22:38:38 -0500

    Added a test using begin() and end()

--- test/iterator_test.cpp
+++ test/iterator_test.cpp
@@ -8,3 +8,22 @@
 
 using namespace snakelinkedlist;
 using std::string;
+
+TEST_CASE("Testing Begin and End") {
+    std::vector<SnakeBodySegment> snake_vector;
+    SnakeBodySegment snakebody1(2);
+    SnakeBodySegment snakebody2(3);
+    SnakeBodySegment snakebody3(4);
+    snake_vector.push_back(snakebody1);
+    snake_vector.push_back(snakebody2);
+    snake_vector.push_back(snakebody3);
+
+    LinkedList<SnakeBodySegment> list1 = LinkedList<SnakeBodySegment>(snake_vector);
+
+    int count = 0;
+    for (auto it = list1.begin(); it != list1.end(); ++it) {
+        count++;
+    }
+
+    REQUIRE(count == list1.size());
+}
